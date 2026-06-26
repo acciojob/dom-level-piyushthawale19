ef87cdbb7bd7a7d4eb005819b10e9709aaa5a0ab
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-06-26 23:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const ele = document.getElementById('value');
+
+let level =0;
+let current  = ele;
+
+while(current.parentElement){
+	level++;
+	current = current.parentElement;
+}
+
+alert(`The level of the element is: ${level}`);
